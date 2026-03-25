b37cda04d33648538593137626fa56a8b36f6d5a
anonymous03032026 <anonymous03032026@gmail.com>
2026-03-25 12:53:49 +0100

    Update run_bootstrap_es.sh

--- bootstrap/run_bootstrap_es.sh
+++ bootstrap/run_bootstrap_es.sh
@@ -10,13 +10,13 @@
 
 
 
-cd /home/fadoua.jouidel-amri/LES_paper_github || exit 1
+cd /home/anonymous03032026/LES_paper_github || exit 1
 
 module purge
 module load Python/3.11.5-GCCcore-13.2.0
 module load libffi/3.4.4-GCCcore-13.2.0
 
-source /home/fadoua.jouidel-amri/venvs/lesnb/bin/activate
+source /home/anonymous03032026/venvs/lesnb/bin/activate
 
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
@@ -28,6 +28,6 @@ echo "LD_LIBRARY_PATH (batch) = $LD_LIBRARY_PATH"
 
 # run: force env export into the srun step
 /usr/bin/time -v \
-  srun --export=ALL /home/fadoua.jouidel-amri/venvs/lesnb/bin/python bootstrap_es.py \
+  srun --export=ALL /home/anonymous03032026/venvs/lesnb/bin/python bootstrap_es.py \
     --seed $SLURM_ARRAY_TASK_ID \
-    --data-dir /home/fadoua.jouidel-amri/LES_paper_github
+    --data-dir /home/anonymous03032026/LES_paper_github
